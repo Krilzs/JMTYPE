7a8083ef5b4e78768d588b5236c0dabbaff54294
mLascurain <matiaslascurain04@gmail.com>
2025-02-03 15:35:23 -0300

    added header

---
+++ src/Pages/Type/Type.tsx
@@ -0,0 +1,10 @@
+import Header from "../../Components/Header/Header.tsx";
+
+const Type = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+export default Type;
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import routes from "./Pages/routes.jsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
+const router = createBrowserRouter(routes);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
